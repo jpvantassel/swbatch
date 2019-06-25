70137cc3bda3d545126695c8fdc0a0bae3280e97
Joseph vantassel <jvantassel@utexas.edu>
2019-06-24 22:51:19 -0500

    :sparkles: Allow up to 2 Targets, Move extract into loop, multiple seeds

--- wrapper.sh
+++ wrapper.sh
@@ -1,28 +1,31 @@
-# GeopsyInvert v1.0.0
+# GeopsyInvert v2.0.0
 # Written by: Joseph Vantassel (jvantassel@utexas.edu)
 # Written on: 5 March 2019
 # Last edit: 4 April 2019
 # This code performs batch inversions that consider mutliple paramaterizations
+# Last edit: 24 June 2019
+# Update to loop over 2 targets, allow for more than 9 seeds, move extraction inside the loop.
 
 # Inputs 
 #workingDirectory 	# Location of current working directory
 
 # Parameters
-Nm=Test		#Trial name (string)
-Sd=2 		#Number (positive integer) of starting seeds to attempt
-It=2 		#Number (positive integer) of iterations to perform
-N0=1000 	#Number (positive integer) of randomly sampled profiles to attempt before first iteration
-Nr=50 		#Number (positive integer) of best profiles to consider when resampling
-Ns=10 		#Number (positive integer) of new profiles to consider after resampling
-Np=10		#Number (positve integer) of profiles to export
-Fnum=128	#Number (positve integer) for number of frequency points in dispersion curve
-Fmin=1 		#Number (positive float) for minimum frequency of dispersion curve in Hz
-Fmax=100	#Number (positve float) for maximum frequency of dispsersion curve in Hz
+#Nm=Test	#Trial name (string)
+#Sd=2 		#Number (positive integer) of starting seeds to attempt
+#It=2 		#Number (positive integer) of iterations to perform
+#N0=10	 	#Number (positive integer) of randomly sampled profiles to attempt before first iteration
+#Nr=5 		#Number (positive integer) of best profiles to consider when resampling
+#Ns=5 		#Number (positive integer) of new profiles to consider after resampling
+#Np=10		#Number (positve integer) of profiles to export
+#Np=-1		#Number (positve integer) of profiles to export
+#Fnum=20 	#Number (positve integer) for number of frequency points in dispersion curve
+#Fmin=1 	#Number (positive float) for minimum frequency of dispersion curve in Hz
+#Fmax=100	#Number (positve float) for maximum frequency of dispsersion curve in Hz
 
 # Setup
-#set -x
-#WRAPPERDIR=$( cd "$( dirname "$0" )" && pwd )
-#cd ${workingDirectory} 
+set -x
+WRAPPERDIR=$( cd "$( dirname "$0" )" && pwd )
+cd ${workingDirectory} 
 
 
 # Setpath to Geopsy Install
@@ -39,41 +42,42 @@ fi
 
 # List of .param files in Params directory
 Plist='Params/*.param'
-echo $Plist
 
 # List of .target files in Targets directory
 Tlist='Targets/*.target'
-echo $Tlist
 
 # Setup the meta-inversion loop
-on=1
-for T in $Tlist ; do 				# Loop through target list
-    if [ $on==1 ]; then				# Only allow one target for now
-        on=0
-        for P in $Plist ; do			# Loop through parameter list
-            echo $P
-	    for Seed in $(seq 1 $Sd); do 	# New inverison for each seed
-                trim=${P:7}
-                trim=${trim%%.*}
-                rep=${Nm}_${trim}_Sd${Seed}
-                dinver -i DispersionCurve -optimization -itmax $It -ns0 $N0 -ns $Ns -nr $Nr -target $T -param $P -o Reports/${rep}.report 2>> Reports/${rep}.log
-	    done
-        done     
-    fi    
+tarcount=0
+for T in $Tlist ; do
+  if [ $tarcount -lt 2 ]; then
+    tarname=${T##*/}
+    tarroot=${tarname%%.*}
+    for P in $Plist ; do
+      for Seed in $(seq 0 $((Sd-1))); do
+        parname=${P##*/}
+        parroot=${parname%%.*}
+        rep=${Nm}_${tarroot}_${parroot}_Sd${Seed}
+	echo "${rep} Start">>Text/Transfer.log
+        dinver -i DispersionCurve -optimization -itmax $It -ns0 $N0 -ns $Ns -nr $Nr -target $T -param $P -o Reports/${rep}.report 2>> Reports/${rep}.log
+        if [ $Np -ge 1 ]; then    
+          gpdcreport -best $Np Reports/${rep}.report | gpdc -R 1 -n $Fnum -min $Fmin -max $Fmax > Text/${rep}_DC.txt 2>>Text/Transfer.log
+          gpdcreport -best $Np Reports/${rep}.report > Text/${rep}_GM.txt 2>>Text/Transfer.log
+        else
+          msft=$((Np*-1))
+          gpdcreport -m $msft Reports/${rep}.report | gpdc -R 1 -n $Fnum -min $Fmin -max $Fmax > Text/${rep}_DC.txt 2>>Text/Transfer.log
+          gpdcreport -m $msft Reports/${rep}.report > Text/${rep}_GM.txt 2>>Text/Transfer.log
+        fi
+	echo "${rep} Complete">>Text/Transfer.log
+      done
+    done     
+  fi
+  tarcount=$((tarcount+1))   
 done
-
-# Write Np lowest misfit profiles to .txt files
-Replist=Reports/*.report
-for Rep in ${Replist}; do
-    flong=${Rep##*/}
-    fname=${flong%%.*}
-    gpdcreport -best $Np ${Rep} | gpdc -R 1 -n $Fnum -min $Fmin -max $Fmax > Text/${fname}_DC.txt 2>>Text/Transfer.log
-    gpdcreport -best $Np ${Rep} > Text/${fname}_GM.txt 2>>Text/Transfer.log
-done
+echo "Job Successful">>Text/Transfer.log
 
 # Callback failure
-#if [ ! $? ]; then
-#        echo "UTinvert exited with an error status. $?" >&2
-#        ${AGAVE_JOB_CALLBACK_FAILURE}
-#        exit
-#fi
+if [ ! $? ]; then
+        echo "UTinvert exited with an error status. $?" >&2
+        ${AGAVE_JOB_CALLBACK_FAILURE}
+        exit
+fi

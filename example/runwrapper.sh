aa12075a9bf66cd6053184dffa25410642839d28
Joseph vantassel <jvantassel@utexas.edu>
2020-05-17 12:28:49 -0500

    :rotating_light: Update tests

--- example/runwrapper.sh
+++ example/runwrapper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -J MyTest0         
-#SBATCH -o MyTest0.o%j
-#SBATCH -e MyTest0.e%j
+#SBATCH -J test_swbatch         
+#SBATCH -o test_swbatch_%j.o
+#SBATCH -e test_swbatch_%j.e
 #SBATCH -p normal
 #SBATCH -N 1
-#SBATCH -n 1 
+#SBATCH -n 68 
 #SBATCH -t 00:30:00
 #SBATCH -A DesignSafe-BCox-UTAu
 #SBATCH --mail-user=jvantassel@utexas.edu
 #SBATCH --mail-type=all
-./GeopsyInvert.sh
+./swbatch.sh

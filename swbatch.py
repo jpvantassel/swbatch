49e22e304f241349cfd2f378a1c5b99e9773332d
Joseph vantassel <jvantassel@utexas.edu>
2021-03-28 22:30:12 -0500

    :construction: Towards working implementation

--- swbatch.py
+++ swbatch.py
@@ -22,32 +22,18 @@ logger = logging.getLogger('swbatch')
 @click.option("--ns0", required=True, type=int, default=10000, help="Number (positive integer) of randomly sampled profiles to attempt before the first Neighborhood-Algorithm iteration. (10000 is recommended)")
 @click.option("--nr", required=True, type=int, default=100, help="Number (positive integer) of best profiles to consider when resampling. (100 is recommended)")
 @click.option("--ns", required=True, type=int, default=200, help="Number (positive integer) of new profiles to consider per Neighborhood-Algorithm iteration. (200 is recommended)")
-@click.option("--ngroundmodel", required=True, type=int, default=100, help="")
-@click.option("--nrayleigh", required=True, type=int, help="")
-@click.option("--nlove", required=True, type=int, help="")
-@click.option("--dcfnum", required=True, type=int,help="")
-@click.option("--dcfmin", required=True, type=float, help="")
-@click.option("--dcfmax", required=True, type=float, help="")
-@click.option("--nellipticity", required=True, type=int, default=0, help="")
-@click.option("--ellfnum", required=True, type=int, default=64, help="")
-@click.option("--ellfmin", required=True, type=float, default=0.2, help="")
-@click.option("--ellfmax", required=True, type=float, default=20, help="")
-def swbatch(name, ntrial=3, it=250, ns0, nr, ns, ngroundmodel, nrayleigh, nlove, nell, dcfnum, dcfmin, dcfmax, nellipticity=0, ellfnum=64, ellfmin=0.2, ellfmax=20):
-    """Perform batch-style surface wave inversion.
-
-    Parameters
-    ----------
-    name : str
-      Analysis name that is brief, memorable, and descriptive. Each
-      output file will begin with this string of characters. No spaces
-      or special characters are permitted.
-    ntrial : int, optional
-      Number of inversion trials to perform per parameterization,
-      default is 3.
-    it : int, optional
-      Number of Neighborhood-Algorithm iterations to perform per
-      inversion, default is 250.
-
+@click.option("--nmodels", required=True, type=int, default=100, help="Number (positive integer) of ground models/dispersion curves/ellipticity curves to export. (100 is recommended)")
+@click.option("--nrayleigh", required=True, type=int, default=1, help="Number (positive integer) of Rayleigh wave modes to export. (1 is recommended)")
+@click.option("--nlove", required=True, type=int, default=1, help="Number (positive integer) of Love wave modes to export. (1 is recommended)")
+@click.option("--dcfmin", required=True, type=float, default=0.2, help="Number (positive float) for minimum frequency of exported dispersion curve(s) in Hz. Selecting a value slightly less than the minimum frequency of your experimental dispersion data is recommended.")
+@click.option("--dcfmax", required=True, type=float, default=20, help="Number (positive float) for maximum frequency of exported dispersion curve(s) in Hz. Selecting a value slightly larger than the maximum frequency of your experimental dispersion data is recommended.")
+@click.option("--dcfnum", required=True, type=int, default=30, help="Number (positive integer) of frequency points in the exported dispersion curve(s). (30 is recommended)")
+@click.option("--nellipticity", required=True, type=int, default=1, help="Number (positive integer) of Rayleigh modes to include in exported ellipticity. (1 is recommended)")
+@click.option("--ellfmin", required=True, type=float, default=0.2, help="Number (positive float) for minimum frequency of exported Rayleigh wave ellipticity curve(s) in Hz. Selecting a value less than the site's resonant frequency is recommended.")
+@click.option("--ellfmax", required=True, type=float, default=20, help="Number (positive float) for maximum frequency of exported Rayleigh wave ellipticity curve(s) in Hz. Selecting a value greater than the site's resonant frequency is recommended.")
+@click.option("--ellfnum", required=True, type=int, default=30, help="Number of frequency points in exported Rayleigh wave ellipticity curve(s). (30 is recommended)")
+def swbatch(name, ntrial=3, it=250, ns0=10000, nr=100, ns=200, nmodels=100, nrayleigh=1, nlove=1, dcfmin=0.2, dcfmax=20, dcfnum=30, nellipticity=1, ellfmin=0.2, ellfmax=20, ellfnum=30):
+    """SWbatch: a tool for performing batch-style surface wave inversions.
     """
     logger.info(f"name = {name}")
 
@@ -78,28 +64,28 @@ def swbatch(name, ntrial=3, it=250, ns0, nr, ns, ngroundmodel, nrayleigh, nlove,
             os.mkdir(_dir)
 
     # # Setup the meta-inversion loop
-    # for target in targets:
-    #   for param in params:
+    for target in targets:
+      for param in params:
+        for trial in range(ntrial):
 
-    #     tarname=${ctar##*/}
-    #     tarroot=${tarname%%.*}
-    #     for cpar in $plist ; do
-    #       for trial in $(seq 0 $((${ntrial}-1))); do
-    #         parname=${cpar##*/}
-    #         parroot=${parname%%.*}
-    #         rep=${name}_${tarroot}_${parroot}_Tr${trial}
-    # 	      echo "${rep} Start">>3_text/transfer.log
-    #         dinver -i DispersionCurve -optimization -itmax ${It} -ns0 ${Ns0} -ns ${Ns} -nr ${Nr} -target ${ctar} -param ${cpar} -o 2_reports/${rep}.report 2>> 2_reports/${rep}.log
-    #         gpdcreport -best ${nprofile} 2_reports/${rep}.report | gpdc -R 1 -n ${fnum} -min ${fmin} -max ${fmax} > 3_text/${rep}_DC.txt 2>>3_text/transfer.log
-    #         gpdcreport -best ${nprofile} 2_reports/${rep}.report > 3_text/${rep}_GM.txt 2>>3_text/transfer.log
-    # 	      echo "${rep} Complete">>3_text/transfer.log
-    #       done
-    #     done
-    #   fi
-    #   tarcount=$((tarcount+1))
-    # done
-    # echo "Job Complete">>3_text/transfer.log
+        logging.info(f"Starting: target={target}, param={param}, trial={trial}")
 
+        out=f"{name}_{target[:-7]}_{param[:-6]}_tr{trial}"
+
+        # dinver -i DispersionCurve -optimization -itmax ${It} -ns0 ${Ns0} -ns ${Ns} -nr ${Nr} -target ${ctar} -param ${cpar} -o 2_reports/${rep}.report 2>> 2_reports/${rep}.log
+        subprocess.run(["dinver", "-i", "DispersionCurve", "-optimization", "-itmax", it, "-ns0", ns0, "-ns", ns, "-nr", nr, "-target", target, "-param", param, "-o", f"2_reports/{out}.report"], check=True)
+
+        # gpdcreport -best ${nprofile} 2_reports/${rep}.report > 3_text/${rep}_GM.txt 2>>3_text/transfer.log
+        with open(f"3_text/{out}_GM.txt", "w") as f:
+          subprocess.run(["gpdcreport", "-best", nmodels, f"2_report/{out}.report"], stdout=f, check=True)
+
+        # gpdc -R 1 -n ${fnum} -min ${fmin} -max ${fmax} > 3_text/${rep}_DC.txt 2>>3_text/transfer.log
+        with open(f"3_text/{out}_DC.txt", "w") as f:
+          subprocess.run(["gpdc", "-R", nrayleigh, "-L", nlove, "-min", dcfmin, "-max", dcfmax, "-n", dcfnum, f"3_text/{out}_GM.txt"], stdout=f, check=True)
+
+        # gpell -R 1 -n ${fnum} -min ${fmin} -max ${fmax} > 3_text/${rep}_DC.txt 2>>3_text/transfer.log
+        with open(f"3_text/{out}_ELL.txt", "w") as f:
+          subprocess.run(["gpell", "-R", nellipticity, "-min", ellfmin, "-max", ellfmax, "-n", ellfnum, f"3_text/{out}_GM.txt"], stdout=f, check=True)
 
 if __name__ == "__main__":
     swbatch()

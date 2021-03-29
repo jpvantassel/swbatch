bf59bf6b371e8020a9426355557831199b1768e7
Joseph Vantassel <jvantassel@utexas.edu>
2021-03-29 16:42:05 -0500

    :bug: Set all intputs as str, at least one more bug

---
+++ example/run.sh
@@ -0,0 +1,52 @@
+# This file belongs to SWbatch: a DesignSafe-CI application
+# for performing batch-style surface-wave inversions.
+# Copyright (C) 2019 - 2021 Joseph P. Vantassel (jvantassel@utexas.edu)
+
+# Useful defaults
+workingdirectory=.
+
+# Setup
+set -x
+WRAPPERDIR=$( cd "$( dirname "$0" )" && pwd )
+cd ${workingdirectory} 
+
+# Useful defaults
+name=test
+ntrial=2
+it=10
+ns0=1000
+nr=100
+ns=10
+nmodels=10
+nrayleigh=1
+nlove=1
+dcfnum=20
+dcfmin=0.2
+dcfmax=20
+nellipticity=1
+ellfmin=0.2
+ellfmax=20
+ellfnum=30
+
+# Load python3
+module load python3
+
+# Install requirements 
+pip3 install --user -r ../requirements.txt
+
+# Setpath to Geopsy Install
+PATH=$PATH:/work/01698/rauta/geopsy/install/bin/
+
+# Launch swbatch
+python3 ../swbatch.py --name ${name} --ntrial ${ntrial} --it ${it}\
+ --ns0 ${ns0} --nr ${nr} --ns ${ns} --nmodels ${nmodels} --nrayleigh ${nrayleigh}\
+ --nlove ${nlove} --dcfmin ${dcfmin} --dcfmax ${dcfmax} --dcfnum ${dcfnum}\
+ --nellipticity ${nellipticity} --ellfmin ${ellfmin} --ellfmax ${ellfmax}\
+ --ellfnum ${ellfnum}
+
+# Callback failure
+if [ ! $? ]; then
+        echo "SWbatch exited with an error status. $?" >&2
+        ${AGAVE_JOB_CALLBACK_FAILURE}
+        exit
+fi
--- swbatch.py
+++ swbatch.py
@@ -17,21 +17,21 @@ logger = logging.getLogger('swbatch')
 
 @click.command()
 @click.option("--name", required=True, type=str, help="Analysis name that is brief, memorable, and descriptive. Each output file will begin with this string of characters. No spaces or special characters are permitted.")
-@click.option("--ntrial", required=True, type=int, default=3, help="Number (positive integer) of inversion trials to perform per parameterization. (3 is recommended)")
-@click.option("--it", required=True, type=int, default=250, help="Number of Neighborhood-Algorithm iterations to perform per inversion. (250 is recommended)")
-@click.option("--ns0", required=True, type=int, default=10000, help="Number (positive integer) of randomly sampled profiles to attempt before the first Neighborhood-Algorithm iteration. (10000 is recommended)")
-@click.option("--nr", required=True, type=int, default=100, help="Number (positive integer) of best profiles to consider when resampling. (100 is recommended)")
-@click.option("--ns", required=True, type=int, default=200, help="Number (positive integer) of new profiles to consider per Neighborhood-Algorithm iteration. (200 is recommended)")
-@click.option("--nmodels", required=True, type=int, default=100, help="Number (positive integer) of ground models/dispersion curves/ellipticity curves to export. (100 is recommended)")
-@click.option("--nrayleigh", required=True, type=int, default=1, help="Number (positive integer) of Rayleigh wave modes to export. (1 is recommended)")
-@click.option("--nlove", required=True, type=int, default=1, help="Number (positive integer) of Love wave modes to export. (1 is recommended)")
-@click.option("--dcfmin", required=True, type=float, default=0.2, help="Number (positive float) for minimum frequency of exported dispersion curve(s) in Hz. Selecting a value slightly less than the minimum frequency of your experimental dispersion data is recommended.")
-@click.option("--dcfmax", required=True, type=float, default=20, help="Number (positive float) for maximum frequency of exported dispersion curve(s) in Hz. Selecting a value slightly larger than the maximum frequency of your experimental dispersion data is recommended.")
-@click.option("--dcfnum", required=True, type=int, default=30, help="Number (positive integer) of frequency points in the exported dispersion curve(s). (30 is recommended)")
-@click.option("--nellipticity", required=True, type=int, default=1, help="Number (positive integer) of Rayleigh modes to include in exported ellipticity. (1 is recommended)")
-@click.option("--ellfmin", required=True, type=float, default=0.2, help="Number (positive float) for minimum frequency of exported Rayleigh wave ellipticity curve(s) in Hz. Selecting a value less than the site's resonant frequency is recommended.")
-@click.option("--ellfmax", required=True, type=float, default=20, help="Number (positive float) for maximum frequency of exported Rayleigh wave ellipticity curve(s) in Hz. Selecting a value greater than the site's resonant frequency is recommended.")
-@click.option("--ellfnum", required=True, type=int, default=30, help="Number of frequency points in exported Rayleigh wave ellipticity curve(s). (30 is recommended)")
+@click.option("--ntrial", required=True, type=str, default=3, help="Number (positive integer) of inversion trials to perform per parameterization. (3 is recommended)")
+@click.option("--it", required=True, type=str, default=250, help="Number of Neighborhood-Algorithm iterations to perform per inversion. (250 is recommended)")
+@click.option("--ns0", required=True, type=str, default=10000, help="Number (positive integer) of randomly sampled profiles to attempt before the first Neighborhood-Algorithm iteration. (10000 is recommended)")
+@click.option("--nr", required=True, type=str, default=100, help="Number (positive integer) of best profiles to consider when resampling. (100 is recommended)")
+@click.option("--ns", required=True, type=str, default=200, help="Number (positive integer) of new profiles to consider per Neighborhood-Algorithm iteration. (200 is recommended)")
+@click.option("--nmodels", required=True, type=str, default=100, help="Number (positive integer) of ground models/dispersion curves/ellipticity curves to export. (100 is recommended)")
+@click.option("--nrayleigh", required=True, type=str, default=1, help="Number (positive integer) of Rayleigh wave modes to export. (1 is recommended)")
+@click.option("--nlove", required=True, type=str, default=1, help="Number (positive integer) of Love wave modes to export. (1 is recommended)")
+@click.option("--dcfmin", required=True, type=str, default=0.2, help="Number (positive float) for minimum frequency of exported dispersion curve(s) in Hz. Selecting a value slightly less than the minimum frequency of your experimental dispersion data is recommended.")
+@click.option("--dcfmax", required=True, type=str, default=20, help="Number (positive float) for maximum frequency of exported dispersion curve(s) in Hz. Selecting a value slightly larger than the maximum frequency of your experimental dispersion data is recommended.")
+@click.option("--dcfnum", required=True, type=str, default=30, help="Number (positive integer) of frequency points in the exported dispersion curve(s). (30 is recommended)")
+@click.option("--nellipticity", required=True, type=str, default=1, help="Number (positive integer) of Rayleigh modes to include in exported ellipticity. (1 is recommended)")
+@click.option("--ellfmin", required=True, type=str, default=0.2, help="Number (positive float) for minimum frequency of exported Rayleigh wave ellipticity curve(s) in Hz. Selecting a value less than the site's resonant frequency is recommended.")
+@click.option("--ellfmax", required=True, type=str, default=20, help="Number (positive float) for maximum frequency of exported Rayleigh wave ellipticity curve(s) in Hz. Selecting a value greater than the site's resonant frequency is recommended.")
+@click.option("--ellfnum", required=True, type=str, default=30, help="Number of frequency points in exported Rayleigh wave ellipticity curve(s). (30 is recommended)")
 def swbatch(name, ntrial=3, it=250, ns0=10000, nr=100, ns=200, nmodels=100, nrayleigh=1, nlove=1, dcfmin=0.2, dcfmax=20, dcfnum=30, nellipticity=1, ellfmin=0.2, ellfmax=20, ellfnum=30):
     """SWbatch: a tool for performing batch-style surface wave inversions.
 
@@ -56,9 +56,11 @@ def swbatch(name, ntrial=3, it=250, ns0=10000, nr=100, ns=200, nmodels=100, nray
 
     # List of .target files in 0_targets directory.
     targets = glob.glob('0_targets/*.target')
+    logger.info(f"targets      = {targets}")
 
     # List of .param files in 1_parameters directory.
     params = glob.glob('1_parameters/*.param')
+    logger.info(f"params       = {params}")
 
     # Create directories if they do not yet exist.
     dirs = ["2_reports", "3_text"]
@@ -69,12 +71,14 @@ def swbatch(name, ntrial=3, it=250, ns0=10000, nr=100, ns=200, nmodels=100, nray
     # Setup the meta-inversion loop.
     for target in targets:
         for param in params:
-            for trial in range(ntrial):
-                logging.info(
+            for trial in range(int(ntrial)):
+                logger.info(
                     f"Starting: target={target}, param={param}, trial={trial}")
 
                 # Create default file name.
-                out = f"{name}_{target[:-7]}_{param[:-6]}_TR{trial}"
+                _, target_suffix = target.split("/")
+                _, param_suffix = param.split("/")
+                out = f"{name}_{target_suffix[:-7]}_{param_suffix[:-6]}_TR{trial}"
 
                 # Perform surface wave inversion.
                 # dinver -i DispersionCurve -optimization -itmax ${It} -ns0 ${Ns0} -ns ${Ns} -nr ${Nr} -target ${ctar} -param ${cpar} -o 2_reports/${rep}.report 2>> 2_reports/${rep}.log

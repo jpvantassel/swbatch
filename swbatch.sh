9555edec64ab02cdfe4eac31725714396ef7e67f
Joseph Vantassel <jvantassel@utexas.edu>
2022-05-10 19:42:25 -0500

    :hammer: remove help check

--- swbatch.sh
+++ swbatch.sh
@@ -12,14 +12,6 @@ cd ${workingdirectory}
 module purge
 module load tacc-singularity
 
-# Quick check of help
-# -------------------
-singularity exec \
---cleanenv --containall \
---bind $PWD:/home/user/analysis/ \
-docker://jpvantassel/swbatch:geopsy-v3.4.2 \
-/home/user/venv/bin/python3 /home/user/swbatch.py --help
-
 # Launch swbatch
 # --------------
 singularity exec \
